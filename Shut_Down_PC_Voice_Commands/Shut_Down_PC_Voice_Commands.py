885cd2ff8e24a46e4e18e52659247ee281bd8ec0
CodeMaster7000 <95772109+CodeMaster7000@users.noreply.github.com>
2023-02-11 17:48:21 +0000

    Update Shut_Down_PC_Voice_Commands.py

--- Shut_Down_PC_Voice_Commands/Shut_Down_PC_Voice_Commands.py
+++ Shut_Down_PC_Voice_Commands/Shut_Down_PC_Voice_Commands.py
@@ -1,7 +1,6 @@
 import os
 import pyttsx3
 import speech_recognition as sr
-
 class Gfg:
     def takeCommands(self):
           r = sr.Recognizer()
@@ -12,33 +11,29 @@ class Gfg:
               try:
                   print("Recognizing...")
                   Query = r.recognize_google(audio, language='en')
-                  print("The query:", Query, "'")
- 
+                  print("'Query:", Query, "'")
               except Exception as e:
                   print(e) 
-                  print("Could you please repeat?")
+                  print("Could you please repeat that?")
                   return "None"
           return Query
-
     def Speak(self, audio):
           engine = pyttsx3.init('sapi5')
           voices = engine.getProperty('voices')
           engine.setProperty('voice', voices[1].id)
           engine.say(audio)
           engine.runAndWait()
-
     def quitSelf(self):
         self.Speak("Do you wish to shut down your computer?")
         take = self.takeCommand()
         choice = take
         if choice == 'yes':
-            print("Shutting down the computer...")
+            print("Shutting down...")
             self.Speak("Shutting down the computer...")
             os.system("shutdown /s /t 30")
         if choice == 'no':
             print("Thank you.")
             self.Speak("Thank you.")
-         
 if __name__ == '__main__':
-    Maam = Gfg()
-    Maam.quitSelf()
+    User = Gfg()
+    User.quitSelf()
